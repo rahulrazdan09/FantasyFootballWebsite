d52f245cc52cc3d359105fad99709e4ece2ba34c
Rahul Razdan <67453299+rahulrazdan09@users.noreply.github.com>
2026-08-14 11:11:41 -0400

    Add ESPN league history

---
+++ espn-history.js
@@ -0,0 +1,606 @@
+export default {
+  "2022": [
+    {
+      "finish": 1,
+      "team": "Muth Crossing",
+      "owner": "Anirudh Iyer",
+      "record": "11-3-0",
+      "pf": 1737.98,
+      "pa": 1610.66,
+      "pfpg": 124.1,
+      "papg": 115,
+      "diff": 9.1
+    },
+    {
+      "finish": 2,
+      "team": "Big Kirko",
+      "owner": "Fazil S",
+      "record": "10-4-0",
+      "pf": 1772.1,
+      "pa": 1597.92,
+      "pfpg": 126.6,
+      "papg": 114.1,
+      "diff": 12.4
+    },
+    {
+      "finish": 3,
+      "team": "Team LeInjuries",
+      "owner": "Rahul Razdan",
+      "record": "6-8-0",
+      "pf": 1701.46,
+      "pa": 1628.9,
+      "pfpg": 121.5,
+      "papg": 116.4,
+      "diff": 5.2
+    },
+    {
+      "finish": 4,
+      "team": "Human Trafficking",
+      "owner": "Shreyas Nair",
+      "record": "9-5-0",
+      "pf": 1563.54,
+      "pa": 1547.04,
+      "pfpg": 111.7,
+      "papg": 110.5,
+      "diff": 1.2
+    },
+    {
+      "finish": 5,
+      "team": "Team Mohammed Bin Staley",
+      "owner": "Anish W",
+      "record": "9-5-0",
+      "pf": 1753.58,
+      "pa": 1532.76,
+      "pfpg": 125.3,
+      "papg": 109.5,
+      "diff": 15.8
+    },
+    {
+      "finish": 6,
+      "team": "Team Chehadeh",
+      "owner": "Ahmad Chehadeh",
+      "record": "7-7-0",
+      "pf": 1687.08,
+      "pa": 1632.46,
+      "pfpg": 120.5,
+      "papg": 116.6,
+      "diff": 3.9
+    },
+    {
+      "finish": 7,
+      "team": "Team Kart",
+      "owner": "Kartikay Joshi",
+      "record": "6-8-0",
+      "pf": 1687.12,
+      "pa": 1688.4,
+      "pfpg": 120.5,
+      "papg": 120.6,
+      "diff": -0.1
+    },
+    {
+      "finish": 8,
+      "team": "Made U Mix Dosi",
+      "owner": "Edwin Joseph",
+      "record": "6-8-0",
+      "pf": 1373.76,
+      "pa": 1490.3,
+      "pfpg": 98.1,
+      "papg": 106.5,
+      "diff": -8.3
+    },
+    {
+      "finish": 9,
+      "team": "Cuckhold Every Year",
+      "owner": "Abhi Kiran",
+      "record": "6-8-0",
+      "pf": 1648.52,
+      "pa": 1712.66,
+      "pfpg": 117.8,
+      "papg": 122.3,
+      "diff": -4.6
+    },
+    {
+      "finish": 10,
+      "team": "What's The Point?",
+      "owner": "Siddhant Dosi",
+      "record": "4-10-0",
+      "pf": 1370.82,
+      "pa": 1478.8,
+      "pfpg": 97.9,
+      "papg": 105.6,
+      "diff": -7.7
+    },
+    {
+      "finish": 11,
+      "team": "Unluck God",
+      "owner": "Puneet Joshi",
+      "record": "5-9-0",
+      "pf": 1429.54,
+      "pa": 1654.92,
+      "pfpg": 102.1,
+      "papg": 118.2,
+      "diff": -16.1
+    },
+    {
+      "finish": 12,
+      "team": "Team Wenbenyema Tank",
+      "owner": "Abdourrahman Ahmed",
+      "record": "5-9-0",
+      "pf": 1608.7,
+      "pa": 1759.38,
+      "pfpg": 114.9,
+      "papg": 125.7,
+      "diff": -10.8
+    }
+  ],
+  "2023": [
+    {
+      "finish": 1,
+      "team": "Domestic Violence",
+      "owner": "Shreyas Nair",
+      "record": "8-6-0",
+      "pf": 1566.54,
+      "pa": 1633.48,
+      "pfpg": 111.9,
+      "papg": 116.7,
+      "diff": -4.8
+    },
+    {
+      "finish": 2,
+      "team": "Two Gurls One Kupp",
+      "owner": "Shashank Madarapu",
+      "record": "9-5-0",
+      "pf": 1683.08,
+      "pa": 1630.38,
+      "pfpg": 120.2,
+      "papg": 116.5,
+      "diff": 3.8
+    },
+    {
+      "finish": 3,
+      "team": "Pollard Express",
+      "owner": "Rahul Razdan",
+      "record": "7-7-0",
+      "pf": 1567.54,
+      "pa": 1420.46,
+      "pfpg": 112,
+      "papg": 101.5,
+      "diff": 10.5
+    },
+    {
+      "finish": 4,
+      "team": "Beligoat",
+      "owner": "Kartikay Joshi",
+      "record": "10-4-0",
+      "pf": 1664.16,
+      "pa": 1592.34,
+      "pfpg": 118.9,
+      "papg": 113.7,
+      "diff": 5.1
+    },
+    {
+      "finish": 5,
+      "team": "Team Chehadeh",
+      "owner": "Ahmad Chehadeh",
+      "record": "7-7-0",
+      "pf": 1583.78,
+      "pa": 1528.44,
+      "pfpg": 113.1,
+      "papg": 109.2,
+      "diff": 4
+    },
+    {
+      "finish": 6,
+      "team": "Team Meri BiJaan",
+      "owner": "Anish W",
+      "record": "8-6-0",
+      "pf": 1584.82,
+      "pa": 1537.08,
+      "pfpg": 113.2,
+      "papg": 109.8,
+      "diff": 3.4
+    },
+    {
+      "finish": 7,
+      "team": "Dose of Dosi",
+      "owner": "Siddhant Dosi",
+      "record": "9-5-0",
+      "pf": 1575.4,
+      "pa": 1437.84,
+      "pfpg": 112.5,
+      "papg": 102.7,
+      "diff": 9.8
+    },
+    {
+      "finish": 8,
+      "team": "I did not cook",
+      "owner": "Fazil S",
+      "record": "5-9-0",
+      "pf": 1604.52,
+      "pa": 1625.56,
+      "pfpg": 114.6,
+      "papg": 116.1,
+      "diff": -1.5
+    },
+    {
+      "finish": 9,
+      "team": "Sir. Deer in Headlights",
+      "owner": "Puneet Joshi",
+      "record": "6-8-0",
+      "pf": 1631.2,
+      "pa": 1685.02,
+      "pfpg": 116.5,
+      "papg": 120.4,
+      "diff": -3.8
+    },
+    {
+      "finish": 10,
+      "team": "The 305 Thrivers",
+      "owner": "Maxwell Levenson",
+      "record": "6-8-0",
+      "pf": 1620.92,
+      "pa": 1659.3,
+      "pfpg": 115.8,
+      "papg": 118.5,
+      "diff": -2.7
+    },
+    {
+      "finish": 11,
+      "team": "Japanese Internment",
+      "owner": "Abdourrahman Ahmed",
+      "record": "6-8-0",
+      "pf": 1610.28,
+      "pa": 1666.3,
+      "pfpg": 115,
+      "papg": 119,
+      "diff": -4
+    },
+    {
+      "finish": 12,
+      "team": "Hock and Ball Torture",
+      "owner": "Anirudh Iyer",
+      "record": "7-7-0",
+      "pf": 1485.26,
+      "pa": 1499.62,
+      "pfpg": 106.1,
+      "papg": 107.1,
+      "diff": -1
+    },
+    {
+      "finish": 13,
+      "team": "Tankformaye",
+      "owner": "Abhi Kiran",
+      "record": "5-9-0",
+      "pf": 1612.14,
+      "pa": 1753.26,
+      "pfpg": 115.2,
+      "papg": 125.2,
+      "diff": -10.1
+    },
+    {
+      "finish": 14,
+      "team": "Bustid By Blaine",
+      "owner": "Edwin Joseph",
+      "record": "5-9-0",
+      "pf": 1408.94,
+      "pa": 1529.5,
+      "pfpg": 100.6,
+      "papg": 109.3,
+      "diff": -8.6
+    }
+  ],
+  "2024": [
+    {
+      "finish": 1,
+      "team": "0.04",
+      "owner": "Edwin Joseph",
+      "record": "8-6-0",
+      "pf": 1806.64,
+      "pa": 1715.28,
+      "pfpg": 129,
+      "papg": 122.5,
+      "diff": 6.5
+    },
+    {
+      "finish": 2,
+      "team": "I Hate Jalen Hurts",
+      "owner": "Puneet Joshi",
+      "record": "7-7-0",
+      "pf": 1621.48,
+      "pa": 1589.62,
+      "pfpg": 115.8,
+      "papg": 113.5,
+      "diff": 2.3
+    },
+    {
+      "finish": 3,
+      "team": "0.04",
+      "owner": "Rahul Razdan",
+      "record": "8-6-0",
+      "pf": 1589.28,
+      "pa": 1719.04,
+      "pfpg": 113.5,
+      "papg": 122.8,
+      "diff": -9.3
+    },
+    {
+      "finish": 4,
+      "team": "TEU",
+      "owner": "Fazil S",
+      "record": "9-5-0",
+      "pf": 1742.46,
+      "pa": 1613.66,
+      "pfpg": 124.5,
+      "papg": 115.3,
+      "diff": 9.2
+    },
+    {
+      "finish": 5,
+      "team": "Japanese Internment",
+      "owner": "Abdourrahman Ahmed",
+      "record": "8-6-0",
+      "pf": 1728.82,
+      "pa": 1676.68,
+      "pfpg": 123.5,
+      "papg": 119.8,
+      "diff": 3.7
+    },
+    {
+      "finish": 6,
+      "team": "Indubitably",
+      "owner": "Siddhant Dosi",
+      "record": "7-7-0",
+      "pf": 1614.86,
+      "pa": 1546.3,
+      "pfpg": 115.3,
+      "papg": 110.5,
+      "diff": 4.9
+    },
+    {
+      "finish": 7,
+      "team": "Sophomore Slump",
+      "owner": "Shashank Madarapu",
+      "record": "7-7-0",
+      "pf": 1694.92,
+      "pa": 1629.16,
+      "pfpg": 121.1,
+      "papg": 116.4,
+      "diff": 4.7
+    },
+    {
+      "finish": 8,
+      "team": "Brown McMonkey",
+      "owner": "Anish W",
+      "record": "6-8-0",
+      "pf": 1521.94,
+      "pa": 1635.06,
+      "pfpg": 108.7,
+      "papg": 116.8,
+      "diff": -8.1
+    },
+    {
+      "finish": 9,
+      "team": "Beligoat",
+      "owner": "Kartikay Joshi",
+      "record": "7-7-0",
+      "pf": 1587.5,
+      "pa": 1433.88,
+      "pfpg": 113.4,
+      "papg": 102.4,
+      "diff": 11
+    },
+    {
+      "finish": 10,
+      "team": "Jaydisney BRobbed Me",
+      "owner": "Abhi Kiran",
+      "record": "7-7-0",
+      "pf": 1464.74,
+      "pa": 1624.96,
+      "pfpg": 104.6,
+      "papg": 116.1,
+      "diff": -11.4
+    },
+    {
+      "finish": 11,
+      "team": "I Cook Lamb",
+      "owner": "Ahmad Chehadeh",
+      "record": "5-9-0",
+      "pf": 1687.72,
+      "pa": 1820.06,
+      "pfpg": 120.6,
+      "papg": 130,
+      "diff": -9.5
+    },
+    {
+      "finish": 12,
+      "team": "OHHH Bashar al-Rachaad",
+      "owner": "Anirudh Iyer",
+      "record": "6-8-0",
+      "pf": 1466.78,
+      "pa": 1495.98,
+      "pfpg": 104.8,
+      "papg": 106.9,
+      "diff": -2.1
+    },
+    {
+      "finish": 13,
+      "team": "Look At Crackers?",
+      "owner": "Maxwell Levenson",
+      "record": "7-7-0",
+      "pf": 1473.52,
+      "pa": 1548.14,
+      "pfpg": 105.3,
+      "papg": 110.6,
+      "diff": -5.3
+    },
+    {
+      "finish": 14,
+      "team": "Enforced Prostitution",
+      "owner": "Shreyas Nair",
+      "record": "6-8-0",
+      "pf": 1661.54,
+      "pa": 1614.38,
+      "pfpg": 118.7,
+      "papg": 115.3,
+      "diff": 3.4
+    }
+  ],
+  "2025": [
+    {
+      "finish": 1,
+      "team": "I hope Edwin gets a third ring!",
+      "owner": "Anirudh Iyer",
+      "record": "9-5-0",
+      "pf": 1770.34,
+      "pa": 1650.9,
+      "pfpg": 126.5,
+      "papg": 117.9,
+      "diff": 8.5
+    },
+    {
+      "finish": 2,
+      "team": "Yamete Kudasai",
+      "owner": "Edwin Joseph",
+      "record": "8-6-0",
+      "pf": 1560.32,
+      "pa": 1585.92,
+      "pfpg": 111.5,
+      "papg": 113.3,
+      "diff": -1.8
+    },
+    {
+      "finish": 3,
+      "team": "J Train",
+      "owner": "Shreyas Nair",
+      "record": "9-5-0",
+      "pf": 1718.92,
+      "pa": 1548.44,
+      "pfpg": 122.8,
+      "papg": 110.6,
+      "diff": 12.2
+    },
+    {
+      "finish": 4,
+      "team": "Puka 2026",
+      "owner": "Siddhant Dosi",
+      "record": "8-6-0",
+      "pf": 1567.56,
+      "pa": 1552.76,
+      "pfpg": 112,
+      "papg": 110.9,
+      "diff": 1.1
+    },
+    {
+      "finish": 5,
+      "team": "Henry Ruggs Racing Inc.",
+      "owner": "Abdourrahman Ahmed",
+      "record": "8-6-0",
+      "pf": 1526.76,
+      "pa": 1402.6,
+      "pfpg": 109.1,
+      "papg": 100.2,
+      "diff": 8.9
+    },
+    {
+      "finish": 6,
+      "team": "Number 1 Raz Glazer",
+      "owner": "Shashank Madarapu",
+      "record": "8-6-0",
+      "pf": 1676.38,
+      "pa": 1632.04,
+      "pfpg": 119.7,
+      "papg": 116.6,
+      "diff": 3.2
+    },
+    {
+      "finish": 7,
+      "team": "Sung Hunter",
+      "owner": "Abhi Kiran",
+      "record": "8-6-0",
+      "pf": 1624.56,
+      "pa": 1495.66,
+      "pfpg": 116,
+      "papg": 106.8,
+      "diff": 9.2
+    },
+    {
+      "finish": 8,
+      "team": "Digg in my Boutte",
+      "owner": "Fazil S",
+      "record": "6-8-0",
+      "pf": 1445.86,
+      "pa": 1498.1,
+      "pfpg": 103.3,
+      "papg": 107,
+      "diff": -3.7
+    },
+    {
+      "finish": 9,
+      "team": "LeInjuries",
+      "owner": "Rahul Razdan",
+      "record": "6-8-0",
+      "pf": 1577.08,
+      "pa": 1627.68,
+      "pfpg": 112.6,
+      "papg": 116.3,
+      "diff": -3.6
+    },
+    {
+      "finish": 10,
+      "team": "I hope AR8 plays really well!",
+      "owner": "Puneet Joshi",
+      "record": "7-7-0",
+      "pf": 1655.3,
+      "pa": 1634.96,
+      "pfpg": 118.2,
+      "papg": 116.8,
+      "diff": 1.5
+    },
+    {
+      "finish": 11,
+      "team": "BELIEVEEEEEE",
+      "owner": "Gordon Chen",
+      "record": "7-7-0",
+      "pf": 1504.02,
+      "pa": 1551.26,
+      "pfpg": 107.4,
+      "papg": 110.8,
+      "diff": -3.4
+    },
+    {
+      "finish": 12,
+      "team": "Beligoat",
+      "owner": "Kartikay Joshi",
+      "record": "7-7-0",
+      "pf": 1559.54,
+      "pa": 1639.82,
+      "pfpg": 111.4,
+      "papg": 117.1,
+      "diff": -5.7
+    },
+    {
+      "finish": 13,
+      "team": "Call Upon The Luck God",
+      "owner": "Anish W",
+      "record": "4-10-0",
+      "pf": 1487.82,
+      "pa": 1679.28,
+      "pfpg": 106.3,
+      "papg": 119.9,
+      "diff": -13.7
+    },
+    {
+      "finish": 14,
+      "team": "Uncle Seymour's Shifty Stars",
+      "owner": "Maxwell Levenson",
+      "record": "3-11-0",
+      "pf": 1431.36,
+      "pa": 1606.4,
+      "pfpg": 102.2,
+      "papg": 114.7,
+      "diff": -12.5
+    }
+  ]
+};
+
+
